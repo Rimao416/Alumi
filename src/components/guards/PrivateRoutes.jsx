f951382dcc6a547cfd2c7adaaaa031ca9124cd79
rimao416 <omarkayumba12345@gmail.com>
2023-12-15 16:15:54 +0100

    Login

--- src/components/guards/PrivateRoutes.jsx
+++ src/components/guards/PrivateRoutes.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function PrivateRoutes() {
   const navigate = useNavigate();
   const isAuthenticated = useSelector((state) => state.authSlice.user?.user);
@@ -15,24 +15,24 @@ function PrivateRoutes() {
       }
     }
   }, [isAuthenticated, navigate]);
-//   if (isAuthenticated) {
-//     const role = isAuthenticated.role;
+  //   if (isAuthenticated) {
+  //     const role = isAuthenticated.role;
 
-//     // En fonction du rôle, redirige vers la route appropriée
-//     switch (role) {
-//       case "Admin":
-//         return <Navigate to="/dashboard" replace />;
-//       case "Prof":
-//         return <Navigate to="/route-b" replace />;
-//       // Ajoutez d'autres cas en fonction des rôles supplémentaires
-//       default:
-//         // Redirige vers une route par défaut ou une page d'erreur si le rôle n'est pas géré
-//         return <Navigate to="/default-route" replace />;
-//     }
-//   } else {
-//     // Redirige vers la page de connexion si l'utilisateur n'est pas authentifié
-//     return <Navigate to="/" />;
-//   }
+  //     // En fonction du rôle, redirige vers la route appropriée
+  //     switch (role) {
+  //       case "Admin":
+  //         return <Navigate to="/dashboard" replace />;
+  //       case "Prof":
+  //         return <Navigate to="/route-b" replace />;
+  //       // Ajoutez d'autres cas en fonction des rôles supplémentaires
+  //       default:
+  //         // Redirige vers une route par défaut ou une page d'erreur si le rôle n'est pas géré
+  //         return <Navigate to="/default-route" replace />;
+  //     }
+  //   } else {
+  //     // Redirige vers la page de connexion si l'utilisateur n'est pas authentifié
+  //     return <Navigate to="/" />;
+  //   }
 }
 
 export default PrivateRoutes;
